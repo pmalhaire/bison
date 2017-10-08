1532d986a26be679ff5ed30858afedd1dbbcf5cc
Pierrot <pierrot@meetlima.com>
2017-10-08 22:49:26 +0200

    some more comments

--- include/bison.hpp
+++ include/bison.hpp
@@ -7,6 +7,14 @@
 
 /*
 main class to use the bson parser
+this class implements the specification v1.1 of the bson spec
+
+how to use loads a bsonfile in memory using a std::vector<char>
+the file MUST be a complete and valid bson file
+invalid bson file will result on hard exit
+
+see unit_tests for the accessor of each type
+
 */
 
 class Bson{
@@ -14,13 +22,13 @@ public:
     Bson() = delete;
     ~Bson();
     Bson(std::vector<char>& vect);
-    std::string dump();
-    BsonDoc* getDoc();
-    void next();
+    BsonDoc* getDoc();               //get the current document as BsonDoc or null if there is no more
+    void next();                     //jump to the next document in the file
+    std::string dump();              //dump a human readable version of the current document as std::string
 private:
-    BsonDoc* _doc = nullptr;
+    BsonDoc*                    _doc = nullptr;
     std::vector<char>::iterator _pos;
-    std::vector<char>& _vect;
+    std::vector<char>&          _vect;
 };
 
 #endif // ndef BISON_HPP
